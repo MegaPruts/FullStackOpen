2d08f3f9e19c49c78d53cee0c36be4594107c22c
MegaPruts <Paul@Rijnvos.net>
2025-01-29 16:39:32 +0100

    1.1 Course Information, step 1

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+const Header = (title) => {
+    return (
+        <h1>{title.title}</h1>
+    )
+}
+const Parts = (exercises) => {
+    return (
+        <>
+            {exercises.exercises.map((e) => <p>{e.part} {e.numberOfExercises}</p>)}
+        </>
+    )
+}
+const NumberOfExercises = (exercises) => {
+    const total = exercises.exercises.map((e) => e.numberOfExercises).reduce((a, b) => a + b)
+    return (
+        <p>Number of exercises {total}</p>
+    )
+}
+const App = () => {
+    const course = {title: 'Half Stack application development'}
+    const exercises = [
+        {part: 'Fundamentals of React', numberOfExercises: 10},
+        {part: 'Using props to pass data', numberOfExercises: 7},
+        {part: 'State of a component', numberOfExercises: 14}]
+    return (
+        <div>
+            <Header title={course.title}/>
+            <Parts exercises={exercises}/>
+            <NumberOfExercises exercises={exercises}/>
+        </div>
+    )
+}
+
+export default App
