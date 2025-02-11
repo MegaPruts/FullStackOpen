702408c468a4502de362e7dc9576439d7ea6f67c
MegaPruts <Paul@Rijnvos.net>
2025-02-11 15:39:41 +0200

    1.7 Unicafe step 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,16 @@ const FeedbackButtons = (props) => {
 }
 
 const Scores = (props) => {
+    const all=props.scores.reduce((a,b)=>a+b)
+    const average=(props.scores[0]-props.scores[2])/2
+    const positive= all===0?0:props.scores[0]/all*100
      return (<div>
         <p>Good: {props.scores[0]}</p>
         <p>Neutral: {props.scores[1]}</p>
-        <p>Bad: {props.scores[2]}</p>
+         <p>Bad: {props.scores[2]}</p>
+         <p>all: {all}</p>
+         <p>average: {average}</p>
+         <p>positive: {positive} %</p>
     </div>)
 }
 
