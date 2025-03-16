39f5729e1171f983ecb38729dc16b302dff2d910
MegaPruts <Paul@Rijnvos.net>
2025-03-16 15:30:20 +0100

    1.2 course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,26 @@
-import {useState} from 'react'
-
-const Title = (props) => {
-    return <h1>{props.title}</h1>
+const Header = (title) => {
+    return (<h1>{title.title}</h1>)
 }
-
-const Button = (props) => {
-    return <button onClick={props.button.onClick}>{props.button.text}</button>
+const Contents = (exercises) => {
+    return exercises.exercises.map((e)=><p key={e.part}>{e.part} {e.numberOfExercises}</p>)
 }
-
-const FeedbackView = (props) => {
-    return (<>
-        <Title title="Give feedback"/>
-        {
-            props.buttons.map(
-                b => <Button button={b}/>
-            )
-        }</>)
+const NumberOfExercises = (exercises) => {
+    const total = exercises.exercises.map((e)=>e.numberOfExercises).reduce((a,b)=>a+b)
+    return (<p>Number of exercises {total}</p>)
 }
-
-const StatisticLine = (props) => {
-    return (<tr>
-        <td>{props.text}</td>
-        <td>{props.value}</td>
-    </tr>)
-}
-
-const Total = (scores) => scores.reduce((a, b) => a + b)
-const Average = (scores) => (scores[0] - scores[1]) / scores.length
-const Positive = (scores) => Total(scores) === 0 ? 0 : scores[0] / Total(scores) * 100
-
-const StatisticsView = (props) => {
-    const scores = props.scores
+const App = () => {
+    const course = {title: 'Half Stack application development'}
+    const exercises = [
+        {part: 'Fundamentals of React', numberOfExercises: 10},
+        {part: 'Using props to pass data', numberOfExercises: 7},
+        {part: 'State of a component', numberOfExercises: 14}]
     return (
         <div>
-            <Title title="Statistics"/>
-            <table>
-                <tbody>
-                {
-                    (Total(scores) === 0)
-                        ? <StatisticLine text="No feedback given"/>
-                        : <>
-                            <StatisticLine text="Good:" value={scores[0]}/>
-                            <StatisticLine text="Neutral:" value={scores[1]}/>
-                            <StatisticLine text="Bad:" value={scores[2]}/>
-                            <StatisticLine text="All:" value={Total(scores)}/>
-                            <StatisticLine text="Average:" value={Average([scores[0], scores[2]])}/>
-                            <StatisticLine text="Positive:" value={Positive(scores) + " %"}/>
-                        </>
-                }
-                </tbody>
-            </table>
+            <Header key='1' title={course.title}/>
+            <Contents key='2' exercises={exercises}/>
+            <NumberOfExercises key='3' exercises={exercises}/>
         </div>
     )
-}
-
-const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
-    const goodButton = {text: "good", onClick: () => setGood(good + 1)}
-    const neutralButton = {text: "neutral", onClick: () => setNeutral(neutral + 1)}
-    const badButton = {text: "bad", onClick: () => setBad(bad + 1)}
-
-
-    return (<div>
-        <FeedbackView buttons={[goodButton, neutralButton, badButton]}/>
-        <StatisticsView scores={[good, neutral, bad]}/>
-    < /div>)
 }
 
 export default App
